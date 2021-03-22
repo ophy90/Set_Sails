df85a8b0bb83fdd813fc7bf86e949cda5fd1409f
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-03-22 17:28:10 +0100

    created table ship + created seeds + controller + method index + view file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Ship.destroy_all
+
+puts 'Creating Ships...'
+ship_1 = Ship.create(name:"titanic", description:"grand navire", category:"paquebot", location: "new York", price: 50000.5)
+puts "Created ship "
+ship_2 = Ship.create(name:"Radeau", description:"grand radeau", category:"bateau", location: "genève", price: 50.5)
+puts "Created ship "
+ship_3 = Ship.create(name:"barque", description:"grande barque", category:"bateau", location: "Lausanne", price: 70.5)
+puts "Created ship "
+
+puts 'Finished!'
