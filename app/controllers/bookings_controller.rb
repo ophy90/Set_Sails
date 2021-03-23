622d426463578b2395c8218de6d1a8c7b9c8c142
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-03-23 17:05:28 +0100

    added method create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,11 +5,22 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
+  def create
+    @ship = Ship.find(params[:ship_id])
+    @booking = Booking.new(booking_params)
+    @booking.ship = @ship
+    @booking.user = current_user
+    if @booking.save
+      redirect_to ship_path(@ship)
+    else
+      render :new
+    end
+  end
 
 private
 
 def booking_params
-  params.require(:booking).permit(:start_date, :end_date, :user_id, :ship_id)
+  params.require(:booking).permit(:start_time, :end_time, :user_id, :ship_id)
 end
 
 end
