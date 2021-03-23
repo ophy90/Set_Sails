ce1db015a15b2f20da919c919c3dea8bf85ac681
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-03-23 16:31:41 +0100

    added controller bookings, method new and add form in view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,15 @@
 class BookingsController < ApplicationController
+  
+  def new
+    @ship = Ship.find(params[:ship_id])
+    @booking = Booking.new
+  end
+
+
+private
+
+def booking_params
+  params.require(:booking).permit(:start_date, :end_date, :user_id, :ship_id)
+end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,6 +1,5 @@
 class Ship < ApplicationRecord
   has_one_attached :photo
   has_many :bookings
-  has_many :users, :through => :bookings 
-  has
+  has_many :users, :through => :bookings
 end
