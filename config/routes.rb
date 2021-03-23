0ed872ca415de0bb73d5d18f034744ea148c2930
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-03-23 17:42:03 +0100

    added route for methode index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :ships, only:[:index, :show, :new, :create] do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :index]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
