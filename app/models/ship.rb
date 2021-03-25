ae4cb41b5047ed514869c86b9972ea4ed685b857
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-03-25 14:49:41 +0100

    modified location in one seed + modified name of column in table ships

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,5 +3,5 @@ class Ship < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :users, :through => :bookings
   geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_location?
 end
