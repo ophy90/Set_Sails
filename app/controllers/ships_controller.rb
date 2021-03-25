b52a233d429a1b2ea0d356eba9750e9bd2803d6c
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-03-25 17:21:33 +0100

    deleted false file + added geocoding in method ships index

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -9,6 +9,14 @@ class ShipsController < ApplicationController
     else
       @ships = Ship.all
    end
+
+   @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { ship: @ship })
+      }
+    end
   end
 
   def show
