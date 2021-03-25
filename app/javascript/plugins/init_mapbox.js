6e90b2e2492006b4cc37f456943adef370f48ebf
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-03-25 16:15:04 +0100

    adapted map size + boundary to marker

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -20,7 +20,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
 };
 
 const initMapbox = () => {
