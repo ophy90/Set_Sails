6109acf68ca2aba472fc5c68c09414feba7b3432
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-03-25 14:01:10 +0100

    added validatin + geocding by in table ships

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,4 +2,6 @@ class Ship < ApplicationRecord
   has_one_attached :photo
   has_many :bookings, dependent: :destroy
   has_many :users, :through => :bookings
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_address?
 end
