9143a38b9c556ae340b88a3f88645f5e1abbf97b
Benjamin Porchet <benjaminporchet.pro@gmail.com>
2021-03-25 11:16:22 +0100

    Filter location index

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'devise'
 
 gem 'cloudinary', '~> 1.16.0'
 
+gem 'pg_search', '~> 2.3.0'
+
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,12 +1,16 @@
 class ShipsController < ApplicationController
   def index
-    @ships = Ship.all
+    if params[:query].present?
+      @ships = Ship.where("location ILIKE ?", "%#{params[:query]}%")
+    else
+      @ships = Ship.all
+    end
   end
 
   def show
     @ship = Ship.find(params[:id])
   end
-  
+
   def new
     @ship = Ship.new
   end
